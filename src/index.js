84e2aa2c758d79cffb872ef5585145c61a269bd9
lawen <lawensardar16@gmail.com>
2022-11-09 15:01:50 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-import reducer from "./features/books/booksSlice";
+
+import rootReducer from "./reducers";
 
 const store = createStore(
-  reducer,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+); 
 
 ReactDOM.render(
   <Provider store={store}>
